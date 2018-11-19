95bd60918c276b40606b475088113e41d30edc18
Guangshan <159384@sh.lianjia.com>
2018-11-19 17:03:45 +0800

    完成到第九章的代码

--- src/main/java/com/mxixm/spring/boot/Starter.java
+++ src/main/java/com/mxixm/spring/boot/Starter.java
@@ -2,16 +2,17 @@ package com.mxixm.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ReflectionUtils;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @SpringBootApplication
 public class Starter {
-
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        SpringApplication.run(Starter.class);
+    // 启动入口，Java原生启动入口
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
     }
 
 }
